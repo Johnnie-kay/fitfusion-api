e4c3e5b979f895221b6c4baa034def9d1b6f57e6
Stanley Joel Nwaegwu <n.stanley@zarttech.com>
2024-11-27 18:15:40 +0100

    implemeted product pagination, delete and tested create product with image upload

--- src/helpers/prisma.js
+++ src/helpers/prisma.js
@@ -2,5 +2,5 @@ const { PrismaClient } = require('@prisma/client')
 const prismaClient = new PrismaClient({
     datasourceUrl: process.env.DATABASE_URL,
 })
-
-module.exports = { prismaClient }
+prismaClient.$connect().then(con => console.log('db connection: ', con)).catch(error => console.log('db connecion error: ', error))
+module.exports = { prismaClient }   
